e45e35e0ec7866734eabbc7158c7531fed6d3504
spriveen <rpeanuka@gmail.com>
2024-04-23 03:04:53 -0700

    update navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Cart from './Pages/Cart/Cart';
+import Placeorder from './Pages/Placeorder/Placeorder';
 import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar/>
+    <div className="App">
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/cart' element={<Cart />} />
+      <Route path='/order' element={<Placeorder />} />
+    </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food hear</h2>
+            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredient and culinary experts.our misssion is satisfy your craving and elevate your dining experience.one delicious meal at a timt.</p>
+            <button>View Menu</button>
+
+        </div>
+
+      
+    </div>
+  )
+}
+
+export default Header
