f08ea033112cc71551d6bfaae4d2eb4851bea316
spriveen <rpeanuka@gmail.com>
2024-04-24 11:30:54 -0700

    update food display

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import'./Home.css'
 import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 
 const Home = () => {
+
+const[category,setCatagory] = useState("All");
+
   return (
     <div>
       <Header/>
+      <ExploreMenu category={category} setCatagory={setCatagory}/>
+      <FoodDisplay  category={category}/>
+
+      
     </div>
   )
 }
